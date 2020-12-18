6def37a17683738344179069f55f40b895460cc7
Rock Boynton <boyntonrl@msoe.edu>
2020-12-18 03:56:46 -0600

    remove debug files

--- lab2_MSP432/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/goali/code/adv-embedded/MSOE_LIB/Debug/MSOE_LIB.lib"
-
