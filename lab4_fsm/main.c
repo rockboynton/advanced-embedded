457276852b5e4be5112d426d2f139461006258d9
Rock Boynton <boyntonrl@msoe.edu>
2021-01-19 20:21:44 -0600

    Add LCD display functionality

--- lab4_fsm/main.c
+++ lab4_fsm/main.c
@@ -29,6 +29,7 @@ back light    (LED, pin 8) not connected
 *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 #include "msp.h"
@@ -44,15 +45,46 @@ back light    (LED, pin 8) not connected
 #define ADC_RANGE 256 // range of a 8-bit adc
 #define TEN_HZ_PSC 18750
 
+typedef struct Input
+{
+    uint8_t val;
+    const uint8_t lcd_x;
+    const uint8_t lcd_y;
+    bool changed;
+} Input;
+
+Input temp = {0, 6, 0, false};
+Input humidity = {0, 10, 1, false};
+Input humidity_setpoint = {0, 10, 2, false};
+Input ice_sensed = {0, 9, 3, false};
+
 // global inputs
-int room_temperature;
-int humidity;
-int humidity_setpoint;
-bool ice_sensed;
+// uint8_t temperature;
+// uint8_t humidity;
+// uint8_t humidity_setpoint;
+// bool ice_sensed;
 
 // global outputs
 bool fan_on;
 bool compressor_on;
+
+// global flags
+// bool humidity_changed;
+// bool temperature_changed;
+// bool setpoint_changed;
+// bool defrost_changed;
+
+/**
+ * @brief Initialize the LCD
+ *
+ */
+void init_lcd(void)
+{
+	LCD_Config();
+	LCD_clear();
+	LCD_home();
+	LCD_contrast(10);
+}
 
 /**
  * @brief Initialize GPIOs
@@ -80,26 +112,63 @@ void init_gpio(void)
     P5->OUT &= ~BIT0;  // set output low to start
 }
 
+void update_display(Input *input)
+{
+    if (input->changed)
+    {
+        LCD_goto_xy(input->lcd_x, input->lcd_y);
+        LCD_print_udec3(input->val);
+
+        input->changed = false;
+    }
+}
+
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    Clock_Init_48MHz(); // run system at 48MHz (default is 3MHz)
 
+    // setup
+	init_gpio();
+//	init_adc();
+//	init_pwm();
+//	init_periodic_timer();
+	init_lcd();
 
+	__enable_interrupts(); // global interrupt enable
+
+    LCD_print_str("Temp:");
+
+	LCD_goto_xy(0, 1);
+	LCD_print_str("Setpt: ");
+
+	LCD_goto_xy(0, 2);
+	LCD_print_str("Humidity:");
+
+    LCD_goto_xy(0, 3);
+	LCD_print_str("Defrost:");
 
 	State current_state = OFF; // start in OFF state
 	Event input_event;
 
     while (1)
 	{
+        __disable_interrupts(); // entering critical section
+        update_display(&temp);
+        update_display(&humidity);
+        update_display(&humidity_setpoint);
+        update_display(&ice_sensed);
+        __enable_interrupts(); // leaving critical section
+
 		// read inputs
-	    if (!ice_sensed && humidity >= humidity_setpoint + 5) {
+	    if (!ice_sensed.val && humidity.val >= humidity_setpoint.val + 5) {
             input_event = HUMIDITY_RISE;
-        } else if (!ice_sensed && humidity >= humidity_setpoint + 5 && !ice_sensed) {
+        } else if (!ice_sensed.val && humidity.val >= humidity_setpoint.val + 5) {
             input_event = HUMIDITY_FALL;
-        } else if (ice_sensed) {
+        } else if (ice_sensed.val) {
             input_event = ICE_SENSED;
-        } else if (!ice_sensed) {
+        } else if (!ice_sensed.val) {
             input_event = IDLE;
         }
 
