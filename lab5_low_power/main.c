52b38c5ddceaa9d4ee6a412dee03db1377e10b6a
Rock Boynton <boyntonrl@msoe.edu>
2021-01-28 22:17:44 -0600

    Got temp sensor working

--- lab5_low_power/main.c
+++ lab5_low_power/main.c
@@ -10,7 +10,7 @@ Description:
 
 *********   Analog Devices TMP36 temperature sensor   ***********************
 Signal    (TMP36)      LaunchPad pin
-V+        (pin 1)      power
++Vs       (pin 1)      power
 Vout      (pin 2)      connected to P4.7
 GND       (pin 3)      ground
 
@@ -41,21 +41,22 @@ back light    (LED, pin 8) not connected
 
 // Defines
 #define TWELVE_BIT_ADC_RANGE 4096
+#define TEN_HZ_PSC 18750
 
 typedef struct Input
 {
-    uint8_t val;
+    float val;
     const uint8_t lcd_x;
     const uint8_t lcd_y;
     bool changed;
 } Input;
 
-Input temp = {0, 9, 0, false};
+Input temp = {0, 8, 0, false};
 
 /**
  * @brief Initialize the LCD
  *
- * Sets it up to display dehumidifier I/O
+ * Sets it up to display temp
  *
  */
 void init_lcd(void)
@@ -104,10 +105,12 @@ void init_gpio(void)
 
 void update_display(Input *input)
 {
+    static char val_str[16];
     if (input->changed)
     {
         LCD_goto_xy(input->lcd_x, input->lcd_y);
-        LCD_print_udec3(input->val);
+        sprintf(val_str, "%.1f", input->val);
+        LCD_print_str(val_str);
         input->changed = false;
     }
 }
@@ -121,12 +124,13 @@ void init_adc(void)
 {
     // Sampling time, S&H=96, ADC14 on, SMCLK, single input, single conv.
 	ADC14->CTL0 |= ADC14_CTL0_SHT0_5 | ADC14_CTL0_SHP | ADC14_CTL0_SSEL_4 | ADC14_CTL0_ON;
-	ADC14->CTL1 &= ~(ADC14_CTL1_RES_2 | ADC14_CTL1_RES_1); // 8-bit conversion
-	ADC14->CTL1 |= (4 << ADC14_CTL1_CSTARTADD_OFS); // use MEM[4]
+	ADC14->CTL1 &= ~(ADC14_CTL1_RES_2 | ADC14_CTL1_RES_1); // clear res bits
+    ADC14->CTL1 |= ADC14_CTL1_RES__12BIT; // set 12 bit resolution
+    ADC14->CTL1 |= (4 << ADC14_CTL1_CSTARTADD_OFS); // use MEM[4]
 	ADC14->MCTL[4] |= ADC14_MCTLN_INCH_6;			// input on A6
 	ADC14->IER0 |= ADC14_IER0_IE4;					// enable interrupt
 	ADC14->CTL0 |= ADC14_CTL0_ENC;
-	NVIC->ISER[0] |= INT_ADC14_BIT; // enable ADC interrupt in NVIC
+	NVIC->ISER[0] |= ADC14_IER0_IE24; // enable ADC interrupt in NVIC
 
     P4->SEL1 |= BIT7;  // give A/D control of pin - A6
     P4->SEL0 |= BIT7;
@@ -155,10 +159,14 @@ void init_periodic_timer(void)
  */
 void ADC14_IRQHandler(void)
 {
-    adc_reading = ADC14->MEM[4];
-    double temp_c = (((float) adc_reading) / TWELVE_BIT_ADC_RANGE) * (125 + 40) + 40; // celcius temp from -40 - 125
-    double temp_f = temp_c * (9.0 / 5.0) + 32;
-	temp.val = (uint8_t) temp_f;
+    uint32_t adc_reading = ADC14->MEM[4];
+    printf("reading (0-4096): %d\t", adc_reading);
+    uint32_t adc_voltage = adc_reading * (3300.0 / TWELVE_BIT_ADC_RANGE);
+    printf("voltage (v): %d\t", adc_voltage);
+    float temp_c = (adc_voltage - 500) / 10.0; // 10mV/C and 500mV offset
+    printf("temp (c): %f\t", temp_c);
+    temp.val = temp_c * 9.0 / 5.0 + 32;
+    printf("temp (f): %f\n", temp.val);
     temp.changed = true;
 }
 
@@ -175,6 +183,7 @@ void TA1_N_IRQHandler(void)
 }
 
 void main(void)
+
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
     // Clock_Init_48MHz(); // run system at 48MHz (default is 3MHz)
@@ -182,9 +191,10 @@ void main(void)
     // setup
 	init_gpio();
 	init_adc();
-	init_lcd();
+    init_periodic_timer();
+    init_lcd();
 
-	__enable_interrupts(); // global interrupt enable
+    __enable_interrupts(); // global interrupt enable
 
     while (1)
 	{
